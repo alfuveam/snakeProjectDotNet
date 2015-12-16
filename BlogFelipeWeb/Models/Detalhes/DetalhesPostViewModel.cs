c6752219120539a49cf190ef9597eab21197272b
felipe borges tomaz <felipe.b.t@hotmail.com>
2015-12-16 19:29:29 -0200

    Adicionado funcionalidades a DetalhesPostViewModel, comandos necessarios para o comentario.

--- BlogFelipeWeb/Models/Detalhes/DetalhesPostViewModel.cs
+++ BlogFelipeWeb/Models/Detalhes/DetalhesPostViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BlogFelipe.DB.Classes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -28,5 +30,25 @@ namespace BlogFelipeWeb.Models.Detalhes
         public bool Visivel { get; set; }
 
         public List<string> Tags { get; set; }
+
+        //* Cadastrar Cometário*/
+        [DisplayName("Nome")]
+        [StringLength(100, ErrorMessage = "O campo nome deve possuir no máximo {1}, Caracteres!")]
+        [Required(ErrorMessage = "O campo Nome é Obrigatório!")]
+        public string ComentarioNome { get; set; }
+
+        [DisplayName("E-mail")]
+        [StringLength(100, ErrorMessage = "O campo e-mail deve possuir no máximo {1}, Caracteres!")]
+        [Required(ErrorMessage = "E-mail inválido")]
+        public string ComentarioEmail { get; set; }
+
+        [DisplayName("Descrição")]        
+        [Required(ErrorMessage = "O campo Descrição é Obrigatorio ")]
+        public string ComentarioDescricao { get; set; }
+
+        [DisplayName("Página Web")]
+        [StringLength(100, ErrorMessage = "O campo Página Web deve possuir no máximo {1} caracteres!")]        
+        public string ComentarioPaginaWeb { get; set; }
+
     }
 }
